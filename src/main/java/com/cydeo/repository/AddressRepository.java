dd5177129e79124e4ce615cd3f1dcc3977bbe84a
fai7h01 <l.varsimashvil10@gmail.com>
2024-05-15 15:30:14 +0400

    cart, address

--- src/main/java/com/cydeo/repository/AddressRepository.java
+++ src/main/java/com/cydeo/repository/AddressRepository.java
@@ -2,13 +2,20 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AddressRepository extends JpaRepository<Address,Long> {
 
     //Write a derived query to get all addresses starting with a street name
+    List<Address> findByStreetStartsWith(String pattern);
 
     //Write a derived query to get top 3 addresses with a specific customer email
+    List<Address> findTop3ByCustomer_Email(String email);
 
     //Write a JPQL query to get all addresses with a specific customerId
+    @Query("select a from Address a where a.customer.id = ?1")
+    List<Address> retrieveAddressWithCustomerId(Long customerId);
 
 }
--- src/main/java/com/cydeo/repository/CartRepository.java
+++ src/main/java/com/cydeo/repository/CartRepository.java
@@ -1,11 +1,16 @@
 package com.cydeo.repository;
 
 import com.cydeo.entity.Cart;
+import com.cydeo.enums.DiscountType;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface CartRepository extends JpaRepository<Cart,Long> {
 
     //Write a derived query to get all cart by specific discount type
+    List<Cart> findByDiscount_DiscountType(DiscountType type);
 
     //Write a JPQL query to get all cart by customer
+    List<Cart> findByCustomer_Id(Long customerId);
 }
