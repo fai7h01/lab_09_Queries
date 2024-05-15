6e4d3e719410f66481724f84a49ab9495f30510a
fai7h01 <l.varsimashvil10@gmail.com>
2024-05-15 15:38:01 +0400

    customer

--- src/main/java/com/cydeo/repository/CustomerRepository.java
+++ src/main/java/com/cydeo/repository/CustomerRepository.java
@@ -2,10 +2,16 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
 
     //Write a derived query to get customer by username
+    List<Customer> findByUserName(String username);
 
     //Write a JPQL query to get customer by email
+    @Query("SELECT c FROM Customer c where c.email = ?1")
+    List<Customer> retrieveByEmail(String email);
 }
