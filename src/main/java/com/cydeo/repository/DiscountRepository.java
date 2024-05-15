b1f356a19395d778946e3f844bc71890ecc60d29
fai7h01 <l.varsimashvil10@gmail.com>
2024-05-15 15:47:32 +0400

    discount

--- src/main/java/com/cydeo/repository/DiscountRepository.java
+++ src/main/java/com/cydeo/repository/DiscountRepository.java
@@ -1,13 +1,22 @@
 package com.cydeo.repository;
 
 import com.cydeo.entity.Discount;
+import com.cydeo.enums.DiscountType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public interface DiscountRepository extends JpaRepository<Discount,Long> {
 
     //Write a derived query to get all discounts greater than discount amount
+    List<Discount> findByDiscountGreaterThan(BigDecimal amount);
 
     //Write a derived query to get all discounts by specific discount type
+    List<Discount> findByDiscountType(DiscountType type);
 
     //Write a JPQL query to get all discounts with the discount amount between given range
+    @Query("select d from Discount d where d.discount between ?1 and ?2")
+    List<Discount> retrieveDiscountInRange(BigDecimal from, BigDecimal end);
 }
