7f04c117704f8c72079de3f773057e60c5eb6863
fai7h01 <l.varsimashvil10@gmail.com>
2024-05-15 15:36:31 +0400

    balance

--- src/main/java/com/cydeo/repository/BalanceRepository.java
+++ src/main/java/com/cydeo/repository/BalanceRepository.java
@@ -2,12 +2,21 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Balance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public interface BalanceRepository extends JpaRepository<Balance,Long> {
 
     //Write a native query to get top 5 max balance
+    @Query(value = "select distinct * from balance where amount is not null order by amount desc limit 5", nativeQuery = true)
+    List<Balance> retrieveTop5MaxBalance();
 
     //Write a derived query to get all balances greater than or equal to specific balance amount
+    List<Balance> findByAmountGreaterThanEqual(BigDecimal amount);
 
     //Write a native query to get all balances less than specific balance amount
+    @Query(value = "select * from balance where amount <= ?1", nativeQuery = true)
+    List<Balance> retrieveLessThan(BigDecimal amount);
 }
